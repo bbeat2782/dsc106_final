2f4c256233a49de6836f994396f3c5c33ca80708
jkaminsky2 <102625537+jkaminsky2@users.noreply.github.com>
2024-03-14 14:19:59 -0700

    Update DifferentYears.svelte

--- src/components/DifferentYears.svelte
+++ src/components/DifferentYears.svelte
@@ -21,6 +21,7 @@
     let filteredResults;
     let filteredStateResults;
     let stateMapStates;
+
     const tooltip = d3.select("body")
         .append("div")
         .attr("class", "tooltip")
@@ -31,7 +32,82 @@
         filteredStateResults = allYearsOverallPres.filter(d => d.year === currentYear);
         plotMap();
         plotStateMap();
+        plotBarPlot();
     });
+
+    const barData = [
+            {year: 2000, candidate: 'Al Gore', electoralVotes: 266, color: 'blue' },
+            {year: 2000, candidate: 'George W. Bush', electoralVotes: 271, color: 'red' },
+            {year: 2004, candidate: 'John Kerry', electoralVotes: 251, color: 'blue' },
+            {year: 2004, candidate: 'George W. Bush', electoralVotes: 286, color: 'red' },
+            {year: 2008, candidate: 'Barack Obama', electoralVotes: 365, color: 'blue' },
+            {year: 2008, candidate: 'John McCain', electoralVotes: 173, color: 'red' },
+            {year: 2012, candidate: 'Barack Obama', electoralVotes: 332, color: 'blue' },
+            {year: 2012, candidate: 'Mitt Romney', electoralVotes: 206, color: 'red' },
+            {year: 2016, candidate: 'Hillary Clinton', electoralVotes: 227, color: 'blue' },
+            {year: 2016, candidate: 'Donald Trump', electoralVotes: 304, color: 'red' },
+            {year: 2020, candidate: 'Joe Biden', electoralVotes: 306, color: 'blue' },
+            {year: 2020, candidate: 'Donald Trump', electoralVotes: 232, color: 'red' }
+        ];
+
+    function plotBarPlot() {
+        const width = 800; // Define width
+
+        const filteredBarData = barData.filter(data => data.year === currentYear);
+        const barWidth = width / (filteredBarData[0].electoralVotes + filteredBarData[1].electoralVotes);
+
+        const svg = d3.select(".chart-container")
+            .insert("svg", ":first-child")
+            .attr("width", width)
+            .attr("height", 100);
+
+        let xPosition = 0;
+        svg.selectAll('rect')
+            .data(filteredBarData)
+            .enter()
+            .append('rect')
+            .attr('x', d => {
+                const xPos = xPosition;
+                xPosition += d.electoralVotes * barWidth;
+                return xPos;
+            })
+            .attr('y', 50)
+            .attr('width', d => d.electoralVotes * barWidth)
+            .attr('height', 25)
+            .attr('fill', d => d.color);
+
+        svg.append("text")
+            .attr("x", 5)
+            .attr("y", 40)
+            .attr("text-anchor", "start")
+            .attr("fill", "black")
+            .text(filteredBarData[0].candidate)
+            .attr("font-size", "18px");
+
+        svg.append("text")
+            .attr("x", width / 2)
+            .attr("y", 40)
+            .attr("text-anchor", "middle")
+            .attr("fill", "black")
+            .text("Goal (270)")
+            .attr("font-size", "18px");
+
+        svg.append("text")
+            .attr("x", width - 5)
+            .attr("y", 40)
+            .attr("text-anchor", "end")
+            .attr("fill", "black")
+            .text(filteredBarData[1].candidate)
+            .attr("font-size", "18px");
+
+        svg.append("line")
+            .attr("x1", width / 2)
+            .attr("y1", 50)
+            .attr("x2", width / 2)
+            .attr("y2", 50 + 30)
+            .attr("stroke", "white")
+            .attr("stroke-width", 3);
+    }
 
     function plotStateMap() {
         const width = 975;
@@ -55,7 +131,6 @@
             .data(topojson.feature(us, us.objects.states).features)
             .join("path")
             .attr("d", path)
-            .attr("cursor", "pointer")
             .attr("fill", d => {
                 const stateName = d.properties.name;
                 const result = resultsMap[stateName];
@@ -94,8 +169,6 @@
             };
         });
 
-        // console.log(resultsMap);
-
         const g = svg.append("g");
 
         const colorScale = d3.scaleLinear()
@@ -111,7 +184,6 @@
             .data(topojson.feature(county, county.objects.counties).features)
             .join("path")
             .attr("d", path)
-            // .attr("cursor", "pointer")
             .attr("fill", d => {
                 const countyName = d.properties.name.toLowerCase();
                 const stateName = state_ids.get(d.id.substring(0,2));
@@ -127,29 +199,12 @@
                         return "black"; // TODO Handle other cases
                     }
                 } catch(err) {
-                    console.log(stateName, countyName);
                     return 'black';
                 }
             })
             .attr("stroke", "white")
             .attr("stroke-width", "0.3px")
-            .attr("stroke-linejoin", "round")
-            .on("mouseover", function(d) {
-                const countyName = d.toElement.__data__.properties.name;
-                const stateName = state_ids.get(d.toElement.__data__.id.substring(0,2));
-                const tooltipText = `${countyName}, ${stateName}`;
-                // Show tooltip
-                tooltip.transition()
-                .duration(200)
-                .style("opacity", .9);
-                tooltip.html(tooltipText);
-            })
-            .on("mouseout", function(d) {
-                // Hide tooltip
-                tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
-            });
+            .attr("stroke-linejoin", "round");
 
         states = g.append("g")
         .selectAll("path")
@@ -196,13 +251,11 @@
                     } else if (winner === "republican") {
                         return redScale(winPercentage);
                     } else {
-                        return "black"; // TODO Handle other cases
+                        return "black"; 
                     }
                 } catch(err) {
                     // these data are missing from the csv file
-                    // 2000 - 2016 Alaska Valdez-cordova
                     // 2000 Colorado broomfield (this county was formed in 2001)
-                    console.log(stateName, countyName);
                     return 'black';
                 }
             });
@@ -226,43 +279,82 @@
         filteredResults = allYearsCountiesResults.filter(result => result.year === currentYear);
         filteredStateResults = allYearsOverallPres.filter(d => d.year === currentYear);
         updateMap();
+        d3.select(".chart-container").select("svg").remove();
+        plotBarPlot();
     }
 </script>
-<div class="map-title">
-    <p>Presidential Election State-Level and County-Level Voting Patterns from 2000 to 2020</p>
-</div>
-<div class="chart-container" style="display: flex;">
-    <div class="states" style="margin-top: 10px; flex: 1;">
-        <svg bind:this={svgNode} />
+<div class="chart-container" style="display: flex; flex-direction: column; margin-top: -40px;">
+    <div class="bar-chart-container" style="display: flex;">
+        <svg class="bar-chart"></svg>
     </div>
-    <div class="states" style="margin-top: 10px; flex: 1;">
-        <svg bind:this={svgNode2} />
-    </div>
-</div>
-<div class="text-box">
-    <p>Visualized above are the presidential election results for every election from 2000 to 2020 (they occur every 4 years). The trend of Republican candidates doing better in rural areas and Democratic candidates in urban areas--mentioned prior--are made apparent here. Since there are more rural than urban areas in the U.S., the county-level election results make it appear that the Republican candidate should win every election by a landslide. But as proved prior, this is not the case because of the use of the electoral college voting system, where state population is taken into account. Now, you can critically look at the county-level and state-level election results and understand how the presidential election outcome is determined.</p>
-</div>
-<input type="range" min="2000" max="2020" step="4" bind:value={currentYear} on:input={updateYear} />
 
+    <div class="map-container" style="display: flex; margin-top: -175px;">
+        <div style="flex: 1;">
+            <svg bind:this={svgNode} class="map"></svg>
+        </div>
+        <div style="flex: 1;">
+            <svg bind:this={svgNode2} class="map"></svg>
+        </div>
+    </div>
+</div>
+
+<div class="text-box" style="margin: 0px auto;">
+    <p style="font-size: 20px; font-weight:bold;">Presidential Election State-Level and County-Level Voting Patterns from 2000 to 2020</p>
+    <p>Visualized are the presidential election results from 2000 to 2020. The trend of Republican candidates doing better in rural areas and Democratic candidates in urban areas is made apparent when comparing the 2020 election results to other elections. Since there are more rural counties and states in the U.S., the county-level and state-level election results make it appear that the Republican candidate should win every election by a landslide. But, as proved prior, this is not the case because of the electoral college voting system, where state population is taken into account. Factoring in county and state population can explain the following scenarios: the Democratic candidate receiving the majority of votes in states with majority Republican-leaning counties and Democratic candidates winning fewer states but receiving more electoral votes (and, thus, winning the election). This leads to closer election results than insinuated by the county-level and state-level depictions. Now, you can critically look at these maps and understand how the election outcome is determined.</p>
+</div>
+<div class="slider">
+    <input type="range" min="2000" max="2020" step="4" bind:value={currentYear} on:input={updateYear} />
+    <p>{currentYear} Presidential Election</p>
+</div>
 <style>
-.text-box {
-  position: absolute;
-  top: 550px; /* Move the text box down by 200 pixels */
-  right: 350px; /* Adjust as needed */
-  padding: 5px;
-  background-color: rgba(255, 255, 255, 0.8);
-  border-radius: 5px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  z-index: 10; /* Ensure the text box is above the map */
-  width: 800px; /* Adjust the width as needed */
-}
-.map-title {
-  position: absolute;
-  top: 40px;
-  left: 24%; /* Adjust the left position as needed */
-  font-size: 20px; /* Adjust the font size as needed */
-  font-weight: bold; /* Adjust the font weight as needed */
-  color: black; /* Adjust the color as needed */
-  z-index: 10; /* Ensure the title is above the map */
-}
+    .chart-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .bar-chart-container {
+        margin-right: 50px;
+    }
+
+    .slider {
+        position: absolute;
+        top: 17px;
+        right: 20px;
+        z-index: 1000;
+    }
+
+    .slider input {
+        margin-right: 50px;
+        margin-top: 65px
+    }
+
+    .slider p {
+        margin-top: -1px;
+        margin-left: -25px;
+        font-weight: bold;
+    }
+
+    .map-container {
+        display: flex;
+        justify-content: space-between;
+        width: 90%;
+        box-sizing: border-box;
+        padding: 10px;
+    }
+
+    .text-box {
+        position: relative;
+        top: -13px;
+        padding: 5px;
+        background-color: rgba(255, 255, 255, 0.8);
+        border-radius:5px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        width: 1160px;
+        text-align: center;
+    }
+
+    .bar-chart-container {
+        margin-right: 50px;
+    }
 </style>
